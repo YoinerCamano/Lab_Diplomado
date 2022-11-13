6f49c11b51cf217faef3ae0e8fe9fc8d8a561c8e
Yoiner Camaño <yoinerelias1996@gmail.com>
2022-11-13 09:35:09 -0500

    Ajuste a Codigo Fuente de proyecto principal

--- K32L2B31A_Lab_Diplomado_IoT/K32L2B31A_Lab_Diplomado_IoT/source/K32L2B31A_Lab_Diplomado_IoT.c
+++ K32L2B31A_Lab_Diplomado_IoT/K32L2B31A_Lab_Diplomado_IoT/source/K32L2B31A_Lab_Diplomado_IoT.c
@@ -1,40 +1,114 @@
-
+/* @File: K32L2B31A_La_Diplomado_IoT.c
+ * @Autor: Yoiner Camaño Quintero
+ * @Version 0.00
+ * @date 05/11/2022
+ * @brief Funcion principal main
+ * @details
+ *
+ *
+ *
+ *
+ * */
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "K32L2B31A.h"
+
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+#include "fsl_adc16.h"
+#include "fsl_common.h"
 
-/* TODO: insert other definitions and declarations here. */
+#include "lpuart0.h"
 
-/*
- * @brief   Application entry point.
- */
+
+uint8_t temp ;
+
+
 int main(void) {
 
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+	BOARD_InitLEDsPins();
+
+	/*Variables*/
+	float voltaje = 0;
+	float voltaje_entrada = 0;
+	float corriente = 0;
+	float lux = 0;
+
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
 #endif
+	/*Configurar ADC para lectura*/
 
-    PRINTF("Hello World \r\n");
+	ADC16_SetChannelConfig(ADC0_PERIPHERAL, ADC0_CH0_CONTROL_GROUP,
+			&ADC0_channelsConfig[0]);
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
+	while (1) {
 
-    while(1) {
+    	if(leer_bandera_nuevo_dato() != 0){
 
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+    		temp = leer_dato();
+
+    		escribir_bandera_nuevo_dato(0);
+
+    		PRINTF("dato es : %c\r\n",temp);
+
+
+    		if(temp == 76){
+
+				ADC16_SetChannelConfig(ADC0_PERIPHERAL, ADC0_CH0_CONTROL_GROUP,
+						&ADC0_channelsConfig[0]);
+
+				/*Espera que el ADC finalice la conversion*/
+
+				while (0U == (kADC16_ChannelConversionDoneFlag& ADC16_GetChannelStatusFlags(ADC0_PERIPHERAL,ADC0_CH0_CONTROL_GROUP)))
+				{
+				}
+				/*captura de dato del ADC, conversion de voltaje, corriente y lux*/
+
+				voltaje_entrada = (ADC16_GetChannelConversionValue(ADC0_PERIPHERAL,ADC0_CH0_CONTROL_GROUP))*(3.3/4095);
+
+				voltaje = 3.30 - voltaje_entrada;
+
+				if (voltaje == 0){
+					corriente = 0;
+					lux = 0;
+				}else{
+					corriente = voltaje/10000;
+					lux = (125/26)*(corriente*10000) + (375/13);
+				}
+				PRINTF("Voltaje es : %2.2fV\r\n",voltaje_entrada);
+				PRINTF("Corriente es : %f uA\r\n",(corriente*10000));
+				PRINTF("Iluminancia es : %f\r\n",(lux));
+    		}
+    		if(temp == 86){
+			GPIO_PortToggle(BOARD_LED1_GPIO, 1u << BOARD_LED1_PIN);
+    		}
+    		if(temp == 82){
+
+			GPIO_PortToggle(BOARD_LED2_GPIO, 1u << BOARD_LED2_PIN);
+    		}
+
+    		if(temp == 118){
+    			GPIO_PortToggle(BOARD_LED1_GPIO, 1u << BOARD_LED1_PIN);
+    		}
+
+    		if (temp == 114){
+    			GPIO_PortToggle(BOARD_LED2_GPIO, 1u << BOARD_LED2_PIN);
+    		}
+
+			__asm volatile ("nop");
+
+    	}
+	}
+	return 0;
+
+
+
 }
